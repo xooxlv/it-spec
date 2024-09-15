6afaab58495492b1492bed1d06e8444f4e67a37f
dx <vl-mr_123@mail.ru>
2024-09-16 00:39:15 +0300

    db

--- db.js
+++ db.js
@@ -1,36 +1,59 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('your_database_name', 'your_username', 'your_password', {
+const sequelize = new Sequelize('postgres', 'postgres', 'root', {
   host: 'localhost',
   dialect: 'postgres',
 });
 
+
 const Appeal = sequelize.define('Appeal', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-}, {
-  tableName: 'appeals',
-});
-
-// Associate the model with the form fields
-Appeal.associate = (models) => {
-  // No associations in this case
-};
-
-module.exports = { sequelize, Appeal };
+    phone: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    task: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        notEmpty : true,
+      }
+    },
+    attachment: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
+  }, {
+    tableName: 'appeals',
+    createdAt: 'created_at',
+    updatedAt: false,
+  });
+  
+  Appeal.sync();
+  
+  Appeal.associate = (models) => {
+  };
+  
+  module.exports = { sequelize, Appeal };
